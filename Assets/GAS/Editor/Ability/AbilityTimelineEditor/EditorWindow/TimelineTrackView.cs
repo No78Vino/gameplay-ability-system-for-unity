4f962f0a701c2b439c3f3a850da098e851a127ac
Victor Fang <390178965@qq.com>
2024-02-27 17:48:54 +0800

    Update TimelineTrackView.cs

--- Assets/GAS/Editor/Ability/AbilityTimelineEditor/EditorWindow/TimelineTrackView.cs
+++ Assets/GAS/Editor/Ability/AbilityTimelineEditor/EditorWindow/TimelineTrackView.cs
@@ -10,11 +10,13 @@ namespace GAS.Editor.Ability.AbilityTimelineEditor
 {
     public class TimelineTrackView
     {
+        private static List<Type> _trackTypeList;
+        private static readonly Dictionary<string, Type> _trackTypeMap = new();
         private readonly VisualElement _root;
         private readonly List<TrackBase> _trackList = new();
+        private Button _btnAddTrack;
         private VisualElement _contentTrackListParent;
         private VisualElement _trackMenuParent;
-        private Button _btnAddTrack;
         public AnimationTrack animationTrack;
 
         public TimelineTrackView(VisualElement root)
@@ -27,31 +29,26 @@ namespace GAS.Editor.Ability.AbilityTimelineEditor
 
         private void InitTracks()
         {
+            _trackTypeList = EditorUtil.FindAllTypesInheritingFrom(typeof(TrackBase));
+            _trackTypeMap.Clear();
+            foreach (var t in _trackTypeList) _trackTypeMap.Add(t.Name, t);
+
+
             _btnAddTrack = _root.Q<Button>("BtnAddTrack");
             _contentTrackListParent = _root.Q<VisualElement>("ContentTrackList");
             _trackMenuParent = _root.Q<VisualElement>("TrackMenu");
-            
+
             _trackMenuParent.AddManipulator(new ContextualMenuManipulator(OnContextMenu));
             _btnAddTrack.clicked += OnClickAddTrack;
-            
+
             RefreshTrackDraw();
             UpdateContentSize();
         }
 
         private void OnClickAddTrack()
         {
-            List<Type> trackTypeList = EditorUtil.FindAllTypesInheritingFrom(typeof(TrackBase));
-            
-            // 显示添加轨道的菜单
             var menu = new GenericMenu();
-
-            foreach (var t in trackTypeList)
-            {
-                menu.AddItem(new GUIContent(t.Name), false, () => CreateTrack(t));
-            }
-            // menu.AddItem(new GUIContent("Animation Track"), false, () => Debug.Log("Add Animation Track"));
-            // menu.AddItem(new GUIContent("Cue Track"), false, () => Debug.Log("Add Cue Track"));
-            
+            foreach (var t in _trackTypeList) menu.AddItem(new GUIContent(t.Name), false, () => CreateTrack(t));
             menu.ShowAsContext();
         }
 
@@ -72,26 +69,33 @@ namespace GAS.Editor.Ability.AbilityTimelineEditor
             foreach (var track in _trackList) track.RefreshShow(Config.FrameUnitWidth);
         }
 
-        // TODO: 右键菜单
         private void OnContextMenu(ContextualMenuPopulateEvent evt)
         {
-            // 添加右键菜单项
-            string[] menuName = { "item1", "item2" };
-            foreach (var n in menuName) evt.menu.AppendAction(n, OnMenuItemClicked, DropdownMenuAction.AlwaysEnabled);
+            foreach (var t in _trackTypeList)
+                evt.menu.AppendAction(t.Name, OnMenuItemClicked, DropdownMenuAction.AlwaysEnabled);
         }
 
         // 右键菜单项点击回调函数
         private void OnMenuItemClicked(DropdownMenuAction action)
         {
-            Debug.Log($"Menu Item Clicked: {action.name}");
+            if (_trackTypeMap.TryGetValue(action.name, out var trackType))
+                CreateTrack(trackType);
         }
-        
+
+        /// <summary>
+        ///     创建新的轨道
+        /// </summary>
+        /// <param name="trackType"></param>
         private void CreateTrack(Type trackType)
         {
+            // TODO:创建Data
+
+            // 创建View
             var track = (TrackBase)Activator.CreateInstance(trackType);
             track.Init(_contentTrackListParent, _trackMenuParent, Config.FrameUnitWidth);
             _trackList.Add(track);
-            Debug.Log("Add " + trackType.Name);
+
+            Debug.Log("[EX] Add a new track:" + trackType.Name);
         }
     }
 }
