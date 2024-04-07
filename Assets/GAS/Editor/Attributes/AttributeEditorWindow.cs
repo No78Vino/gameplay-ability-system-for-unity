49a6e0b34f4177091828cf429d3fa6e550a6dd9c
kenkinky <kenkinky@vip.qq.com>
2024-04-07 15:31:34 +0800

    优化Editor使用体验

--- Assets/GAS/Editor/Attributes/AttributeEditorWindow.cs
+++ Assets/GAS/Editor/Attributes/AttributeEditorWindow.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System.Text;
 using GAS.Editor.Validation;
 using UnityEditor;
 using UnityEngine;
@@ -20,9 +20,15 @@ namespace GAS.Editor
                 Name = name ?? "";
                 Comment = comment ?? "He is very lazy and left nothing behind.";
             }
+
+            public override string ToString()
+            {
+                return $"Name: {Name}, Comment: {Comment}";
+            }
         }
 
-        Data _data;
+        Data _oldData;
+        Data _newData;
         private IEnumerable<string> _nameBlackList;
         private Action<Data> _callback;
 
@@ -38,7 +44,8 @@ namespace GAS.Editor
         private void Init(Data data, IEnumerable<string> nameBlackList,
             Action<Data> callback)
         {
-            _data = data;
+            _oldData = data;
+            _newData = new Data(data.Name, data.Comment);
             _nameBlackList = nameBlackList;
             _callback = callback;
         }
@@ -46,30 +53,72 @@ namespace GAS.Editor
 
         private void OnGUI()
         {
-            _data.Name = EditorGUILayout.TextField($"Attribute Name:", _data.Name);
-            _data.Comment = EditorGUILayout.TextField($"Comment:", _data.Comment);
+            _newData.Name = EditorGUILayout.TextField($"Attribute Name:", _newData.Name);
+            _newData.Comment = EditorGUILayout.TextField($"Comment:", _newData.Comment);
 
             EditorGUILayout.Space();
 
-
             if (GUILayout.Button("Ok"))
             {
-                var validationResult = Validations.ValidateVariableName(_data.Name);
-                if (!validationResult.IsValid)
-                {
-                    EditorUtility.DisplayDialog("Name error", validationResult.Message, "OK");
-                    return;
-                }
-
-                if (_nameBlackList.Contains(_data.Name))
-                {
-                    EditorUtility.DisplayDialog("Name error", "The name already exists!", "OK");
-                    return;
-                }
-
-                _callback?.Invoke(_data);
-                Close();
+                Save();
             }
+
+            EditorGUILayout.HelpBox("Press Enter to save, Esc to cancel", MessageType.None);
+
+            if (Event.current.isKey && Event.current.keyCode == KeyCode.Return && Event.current.type == EventType.KeyUp)
+            {
+                Event.current.Use();
+                Save();
+            }
+
+            if (Event.current.isKey && Event.current.keyCode == KeyCode.Escape && Event.current.type == EventType.KeyUp)
+            {
+                Event.current.Use();
+                if (EditorUtility.DisplayDialog("Warning", "Are you sure to cancel?", "Yes", "No"))
+                {
+                    Close();
+                }
+            }
+        }
+
+        private void Save()
+        {
+            // name is empty means create a new attribute
+            // if name is not empty, show the warning dialog
+            if (_oldData.Name != "" && (_oldData.Name != _newData.Name || _oldData.Comment != _newData.Comment))
+            {
+                var sb = new StringBuilder();
+                if (_oldData.Name != _newData.Name)
+                {
+                    sb.AppendLine($"- name: \"{_oldData.Name}\" => \"{_newData.Name}\"");
+                }
+
+                if (_oldData.Comment != _newData.Comment)
+                {
+                    sb.AppendLine($"- comment: \"{_oldData.Comment}\" => \"{_newData.Comment}\"");
+                }
+
+                if (!EditorUtility.DisplayDialog("The following changes will be made:", sb.ToString(), "Yes", "No"))
+                {
+                    return;
+                }
+            }
+
+            var validationResult = Validations.ValidateVariableName(_newData.Name);
+            if (!validationResult.IsValid)
+            {
+                EditorUtility.DisplayDialog("Name error", validationResult.Message, "OK");
+                return;
+            }
+
+            if (_nameBlackList.Contains(_newData.Name))
+            {
+                EditorUtility.DisplayDialog("Name error", "The name already exists!", "OK");
+                return;
+            }
+
+            _callback?.Invoke(_newData);
+            Close();
         }
     }
 }
--- Assets/GAS/Editor/General/StringEditWindow.cs
+++ Assets/GAS/Editor/General/StringEditWindow.cs
@@ -9,6 +9,7 @@ namespace GAS.Editor.General
     public class StringEditWindow : EditorWindow
     {
         private string _tip;
+        private string _initialString;
         private string _editedString = "";
         private ValidationDelegate _validator;
         private System.Action<string> _callback;
@@ -36,25 +37,54 @@ namespace GAS.Editor.General
 
             if (GUILayout.Button("Save"))
             {
-                if (_validator != null)
-                {
-                    var validationResult = _validator.Invoke(_editedString);
-                    if (!validationResult.IsValid)
-                    {
-                        EditorUtility.DisplayDialog("Validation Failed", validationResult.Message, "OK");
-                        return;
-                    }
-                }
+                Save();
+            }
 
-                _callback?.Invoke(_editedString);
+            EditorGUILayout.HelpBox("Press Enter to save, Esc to cancel", MessageType.None);
+
+            if (Event.current.isKey && Event.current.keyCode == KeyCode.Return && Event.current.type == EventType.KeyUp)
+            {
+                Event.current.Use();
+                Save();
+            }
+
+            if (Event.current.isKey && Event.current.keyCode == KeyCode.Escape && Event.current.type == EventType.KeyUp)
+            {
+                Event.current.Use();
                 Close();
             }
+        }
+
+        private void Save()
+        {
+            if (!string.IsNullOrWhiteSpace(_initialString) && (_initialString != _editedString))
+            {
+                if (!EditorUtility.DisplayDialog("Warning",
+                        $"Are you sure to save the changes?\n\n    {_initialString} => {_editedString}", "Yes", "No"))
+                {
+                    return;
+                }
+            }
+
+            if (_validator != null)
+            {
+                var validationResult = _validator.Invoke(_editedString);
+                if (!validationResult.IsValid)
+                {
+                    EditorUtility.DisplayDialog("Validation Failed", validationResult.Message, "OK");
+                    return;
+                }
+            }
+
+            _callback?.Invoke(_editedString);
+            Close();
         }
 
         private void Init(string tip, string initialString, ValidationDelegate validator,
             System.Action<string> callback)
         {
             _tip = tip;
+            _initialString = initialString;
             _editedString = initialString;
             _validator = validator;
             _callback = callback;
