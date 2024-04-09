85e00df07c61e01fb3e30e58593ab7022a601c8e
kenkinky <kenkinky@vip.qq.com>
2024-04-09 11:10:01 +0800

    修改 ScalableFloatModCalculation 的默认值

--- Assets/GAS/Runtime/Effects/Modifier/ScalableFloatModCalculation.cs
+++ Assets/GAS/Runtime/Effects/Modifier/ScalableFloatModCalculation.cs
@@ -3,12 +3,12 @@
 namespace GAS.Runtime
 {
     [CreateAssetMenu(fileName = "ScalableFloatModCalculation", menuName = "GAS/MMC/ScalableFloatModCalculation")]
-    public class ScalableFloatModCalculation:ModifierMagnitudeCalculation
+    public class ScalableFloatModCalculation : ModifierMagnitudeCalculation
     {
-        [SerializeField] private float k;
-        [SerializeField] private float b;
+        [SerializeField] private float k = 1f;
+        [SerializeField] private float b = 0f;
 
-        public override float CalculateMagnitude(GameplayEffectSpec spec,float input)
+        public override float CalculateMagnitude(GameplayEffectSpec spec, float input)
         {
             return input * k + b;
         }
