f285aafce36ae0881ad5c2392a2ec25aee17b3e2
kenkinky <kenkinky@vip.qq.com>
2024-04-09 11:23:30 +0800

    ScalableFloatModCalculation 添加编辑辅助提示信息

--- Assets/GAS/Runtime/Effects/Modifier/ScalableFloatModCalculation.cs
+++ Assets/GAS/Runtime/Effects/Modifier/ScalableFloatModCalculation.cs
@@ -1,11 +1,19 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace GAS.Runtime
 {
     [CreateAssetMenu(fileName = "ScalableFloatModCalculation", menuName = "GAS/MMC/ScalableFloatModCalculation")]
     public class ScalableFloatModCalculation : ModifierMagnitudeCalculation
     {
-        [SerializeField] private float k = 1f;
+        private const string Desc = "计算公式：ModifierMagnitude * k + b";
+
+        private const string Detail =
+            "ScalableFloatModCalculation：可缩放浮点数计算\n该类型是根据Magnitude计算Modifier模值的，计算公式为：ModifierMagnitude * k + b 实际上就是一个线性函数，k和b为可编辑参数，可以在编辑器中设置。";
+
+        [DetailedInfoBox(Desc, Detail, InfoMessageType.Info)] [SerializeField]
+        private float k = 1f;
+
         [SerializeField] private float b = 0f;
 
         public override float CalculateMagnitude(GameplayEffectSpec spec, float input)
