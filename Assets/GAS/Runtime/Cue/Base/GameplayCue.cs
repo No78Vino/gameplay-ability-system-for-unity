00467b8c428b08c85ba1f1c48f00e2533c92ec80
kenkinky <kenkinky@vip.qq.com>
2024-04-15 14:18:29 +0800

    优化Cue编辑界面排版

--- Assets/GAS/Runtime/Cue/Base/GameplayCue.cs
+++ Assets/GAS/Runtime/Cue/Base/GameplayCue.cs
@@ -10,23 +10,34 @@ namespace GAS.Runtime
 {
     public abstract class GameplayCue : ScriptableObject
     {
-        private const string BaseInfoGroup = "BaseInfoGroup";
-        private const string TagBoxGroup = "TagBoxGroup";
+        private const string TopGroup = "TopGroup";
+        private const string TopGroup_H = "TopGroup/H";
+        private const string TopGroup_H_Left = "TopGroup/H/Left";
+        private const string TopGroup_H_Right = "TopGroup/H/Right";
+        
         
         [Title("基本信息")]
-        [BoxGroup(BaseInfoGroup,false)]
+        [BoxGroup(TopGroup,false)]
+        [HorizontalGroup(TopGroup_H, Width = 250)]     
+        [VerticalGroup(TopGroup_H_Left)]
         [InfoBox("基本信息仅仅是为了备注以方便理解, 不会对游戏产生实质性影响。",SdfIconType.None)]
         public string Name = "Unnamed";
-        [BoxGroup(BaseInfoGroup,false)]
+        
+        [VerticalGroup(TopGroup_H_Left)]
+        [Title("Description", bold: false)]
+        [HideLabel]
+        [MultiLineProperty(5)]
         public string Description = "He is very lazy and left nothing behind.";
         
         [Title("标签")]
-        [BoxGroup(TagBoxGroup,false)]
+        [HorizontalGroup(TopGroup_H, PaddingLeft = 0.025f)]
+        [VerticalGroup(TopGroup_H_Right)]
         [ListDrawerSettings(Expanded = true)]
         [ValueDropdown("TagChoices",HideChildProperties = true)]
         public GameplayTag[] RequiredTags;
         
-        [BoxGroup(TagBoxGroup,false)]
+        [VerticalGroup(TopGroup_H_Right)]
+        [HideLabel]
         [ListDrawerSettings(Expanded = true)]
         [ValueDropdown("TagChoices",HideChildProperties = true)]
         public GameplayTag[] ImmunityTags;
