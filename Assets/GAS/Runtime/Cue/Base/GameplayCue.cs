fc4b68e599c5f3158084edad333cf09c53a8f083
kenkinky <kenkinky@vip.qq.com>
2024-05-17 15:11:03 +0800

    Cue添加类型名称显示及选中/编辑脚本功能

--- Assets/GAS/Runtime/Cue/Base/GameplayCue.cs
+++ Assets/GAS/Runtime/Cue/Base/GameplayCue.cs
@@ -6,73 +6,109 @@ using System.Reflection;
 using GAS.General;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Object = UnityEngine.Object;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace GAS.Runtime
 {
     public abstract class GameplayCue : ScriptableObject
     {
+        private const string ScriptGroup = "ScriptGroup";
+        private const string ScriptGroup_H = "ScriptGroup/H";
         private const string TopGroup = "TopGroup";
         private const string TopGroup_H = "TopGroup/H";
         private const string TopGroup_H_Left = "TopGroup/H/Left";
         private const string TopGroup_H_Right = "TopGroup/H/Right";
-        
-        
+
+#if UNITY_EDITOR
+        [BoxGroup(ScriptGroup, false)]
+        [HorizontalGroup(ScriptGroup_H)]
+        [ShowInInspector]
+        [LabelText("脚本类型")]
+        public string TypeName => GetType().Name;
+
+        [HorizontalGroup(ScriptGroup_H)]
+        [Button("选中脚本")]
+        private void SelectScript()
+        {
+            var monoScript = MonoScript.FromScriptableObject(this);
+            string path = AssetDatabase.GetAssetPath(monoScript);
+
+            var obj = AssetDatabase.LoadAssetAtPath<Object>(path);
+            EditorGUIUtility.PingObject(obj);
+            Selection.activeObject = obj;
+        }
+
+        [HorizontalGroup(ScriptGroup_H)]
+        [Button("编辑脚本")]
+        private void EditScript()
+        {
+            var monoScript = MonoScript.FromScriptableObject(this);
+
+            AssetDatabase.OpenAsset(monoScript);
+        }
+#endif
+
         [Title("基本信息")]
-        [BoxGroup(TopGroup,false)]
-        [HorizontalGroup(TopGroup_H, Width = 250)]     
+        [BoxGroup(TopGroup, false)]
+        [HorizontalGroup(TopGroup_H, Width = 250)]
         [VerticalGroup(TopGroup_H_Left)]
-        [InfoBox("基本信息仅仅是为了备注以方便理解, 不会对游戏产生实质性影响。",SdfIconType.None)]
+        [InfoBox("基本信息仅仅是为了备注以方便理解, 不会对游戏产生实质性影响。", SdfIconType.None)]
         public string Name = "Unnamed";
-        
+
         [VerticalGroup(TopGroup_H_Left)]
         [Title("Description", bold: false)]
         [HideLabel]
         [MultiLineProperty(5)]
         public string Description = "He is very lazy and left nothing behind.";
-        
+
         [Title("标签")]
         [HorizontalGroup(TopGroup_H, PaddingLeft = 0.025f)]
         [VerticalGroup(TopGroup_H_Right)]
         [ListDrawerSettings(Expanded = true)]
-        [ValueDropdown("TagChoices",HideChildProperties = true)]
+        [ValueDropdown("TagChoices", HideChildProperties = true)]
         public GameplayTag[] RequiredTags;
-        
+
         [VerticalGroup(TopGroup_H_Right)]
         [ListDrawerSettings(Expanded = true)]
-        [ValueDropdown("TagChoices",HideChildProperties = true)]
+        [ValueDropdown("TagChoices", HideChildProperties = true)]
         public GameplayTag[] ImmunityTags;
 
         public virtual bool Triggerable(AbilitySystemComponent owner)
         {
             if (owner == null) return false;
             // 持有【所有】RequiredTags才可触发
-            if(!owner.HasAllTags(new GameplayTagSet(RequiredTags))) 
+            if (!owner.HasAllTags(new GameplayTagSet(RequiredTags)))
                 return false;
-            
+
             // 持有【任意】ImmunityTags不可触发
             if (owner.HasAnyTags(new GameplayTagSet(ImmunityTags)))
                 return false;
 
             return true;
         }
-        
+
 #if UNITY_EDITOR
         private static IEnumerable TagChoices = new ValueDropdownList<GameplayTag>();
-        
+
         private void OnEnable()
         {
             SetTagChoices();
         }
-        
+
         private static void SetTagChoices()
         {
             Type gameplayTagSumCollectionType = TypeUtil.FindTypeInAllAssemblies("GAS.Runtime.GTagLib");
-            if(gameplayTagSumCollectionType == null)
+            if (gameplayTagSumCollectionType == null)
             {
                 Debug.LogError("[EX] Type 'GTagLib' not found. Please generate the TAGS CODE first!");
                 TagChoices = new ValueDropdownList<GameplayTag>();
                 return;
             }
+
             FieldInfo tagMapField = gameplayTagSumCollectionType.GetField("TagMap", BindingFlags.Public | BindingFlags.Static);
 
             if (tagMapField != null)
@@ -80,7 +116,7 @@ namespace GAS.Runtime
                 Dictionary<string, GameplayTag> tagMapValue = (Dictionary<string, GameplayTag>)tagMapField.GetValue(null);
                 var tagChoices = tagMapValue.Values.ToList();
                 var choices = new ValueDropdownList<GameplayTag>();
-                foreach (var tag in tagChoices) choices.Add(tag.Name,tag);
+                foreach (var tag in tagChoices) choices.Add(tag.Name, tag);
                 TagChoices = choices;
             }
             else
@@ -90,7 +126,7 @@ namespace GAS.Runtime
         }
 #endif
     }
-    
+
     public abstract class GameplayCue<T> : GameplayCue where T : GameplayCueSpec
     {
         public abstract T CreateSpec(GameplayCueParameters parameters);
