2864520eaf87f30b807eeaaa08776024ece89438
Ex-Hard <390178965@qq.com>
2024-06-12 23:43:04 +0800

    [fix] editor的inspector环境内不可以使用缓存数据

--- Assets/GAS/Runtime/Ability/TimelineAbility/TrackItem/ReleaseGameplayEffectTrackData.cs
+++ Assets/GAS/Runtime/Ability/TimelineAbility/TrackItem/ReleaseGameplayEffectTrackData.cs
@@ -57,7 +57,7 @@ namespace GAS.Runtime
             };
         }
 
-        private TargetCatcherBase LoadTargetCatcher()
+        public TargetCatcherBase LoadTargetCatcher()
         {
             TargetCatcherBase targetCatcher = null;
             var jsonData = jsonTargetCatcher.Data;
@@ -79,7 +79,6 @@ namespace GAS.Runtime
             }
             else
             {
-                Profiler.BeginSample($"{nameof(ReleaseGameplayEffectMarkEvent)}::LoadTargetCatcher() -> Activator.CreateInstance GC Alloc");
                 if (string.IsNullOrEmpty(jsonData))
                 {
                     targetCatcher = Activator.CreateInstance(type) as TargetCatcherBase;
@@ -88,8 +87,6 @@ namespace GAS.Runtime
                 {
                     targetCatcher = JsonUtility.FromJson(jsonData, type) as TargetCatcherBase;
                 }
-
-                Profiler.EndSample();
             }
 
             return targetCatcher;
