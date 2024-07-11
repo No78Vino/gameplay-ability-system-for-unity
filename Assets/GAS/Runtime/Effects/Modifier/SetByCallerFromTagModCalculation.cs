78f00099794ae7f6acc203d22a5d7fe4f9a3f6e7
kenkinky <kenkinky@vip.qq.com>
2024-07-11 11:35:15 +0800

    SetByCallerFromTagModCalculation 提供tag选择列表

--- Assets/GAS/Runtime/Effects/Modifier/SetByCallerFromTagModCalculation.cs
+++ Assets/GAS/Runtime/Effects/Modifier/SetByCallerFromTagModCalculation.cs
@@ -1,16 +1,21 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace GAS.Runtime
 {
     [CreateAssetMenu(fileName = "SetByCallerFromTag", menuName = "GAS/MMC/SetByCallerFromTagModCalculation")]
-    public class SetByCallerFromTagModCalculation:ModifierMagnitudeCalculation
+    public class SetByCallerFromTagModCalculation : ModifierMagnitudeCalculation
     {
-        [SerializeField] private GameplayTag _tag;
-        public override float CalculateMagnitude(GameplayEffectSpec spec  ,float input)
+        [SerializeField]
+        [ValueDropdown("@ValueDropdownHelper.GameplayTagChoices", HideChildProperties = true)]
+        private GameplayTag _tag;
+
+        public override float CalculateMagnitude(GameplayEffectSpec spec, float input)
         {
             var value = spec.GetMapValue(_tag);
 #if UNITY_EDITOR
-            if(value==null) Debug.LogWarning($"[EX] SetByCallerModCalculation: GE's '{_tag.Name}' value(tag map) is not set");
+            if (value == null)
+                Debug.LogWarning($"[EX] SetByCallerModCalculation: GE's '{_tag.Name}' value(tag map) is not set");
 #endif
             return value ?? 0;
         }
