ae2a3ddc8627f8744ab7e6d23bc138de912f84b6
ex-hard <390178965@qq.com>
2024-10-08 15:42:11 +0800

    [feat] 确认放弃SO存储MMC，改为GE内自定义MMC参数

---
+++ Assets/GAS/RuntimeWithECS/Modifier/CommonUsage/MMCScalableFloat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using Unity.Entities;
+using UnityEngine;
+
+namespace GAS.RuntimeWithECS.Modifier.CommonUsage
+{
+    [Serializable]
+    public class MMCScalableFloat:ModMagnitudeCalculation
+    {
+        private const string Desc = "计算公式：ModifierMagnitude * k + b";
+
+        private const string Detail =
+            "ScalableFloatModCalculation：可缩放浮点数计算\n该类型是根据Magnitude计算Modifier模值的，计算公式为：ModifierMagnitude * k + b 实际上就是一个线性函数，k和b为可编辑参数，可以在编辑器中设置。";
+
+        [DetailedInfoBox(Desc, Detail, InfoMessageType.Info), SerializeField]
+        private float k = 1f;
+
+        [SerializeField] private float b;
+        
+        public override float CalculateMagnitude(Entity specEntity, float magnitude)
+        {
+            return magnitude * k + b;
+        }
+
+        public override void InitParameters(float[] floatParams,int[] intParams,string[] stringParams)
+        {
+            k = floatParams[0];
+            b = floatParams[1];
+        }
+    }
+}
---
+++ Assets/GAS/RuntimeWithECS/Modifier/ModMagnitudeCalculation.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using GAS.General;
+using Sirenix.OdinInspector;
+using Unity.Entities;
+using UnityEngine;
+
+namespace GAS.RuntimeWithECS.Modifier
+{
+    public abstract class ModMagnitudeCalculation
+    {
+        protected const int WIDTH_LABEL = 70;
+
+        [TitleGroup("Base")]
+        [HorizontalGroup("Base/H1", width: 1 - 0.618f)]
+        [TabGroup("Base/H1/V1", "Summary", SdfIconType.InfoSquareFill, TextColor = "#0BFFC5", Order = 1)]
+        [HideLabel]
+        [MultiLineProperty(10)]
+        public string Description;
+
+#if UNITY_EDITOR
+        [TabGroup("Base/H1/V2", "General", SdfIconType.AwardFill, TextColor = "#FF7F00", Order = 2)]
+        [TabGroup("Base/H1/V2", "Detail", SdfIconType.TicketDetailedFill, TextColor = "#BC2FDE")]
+        [LabelText("类型名称", SdfIconType.FileCodeFill)]
+        [LabelWidth(WIDTH_LABEL)]
+        [ShowInInspector]
+        [PropertyOrder(-1)]
+        public string TypeName => GetType().Name;
+
+        [TabGroup("Base/H1/V2", "Detail")]
+        [LabelText("类型全名", SdfIconType.FileCodeFill)]
+        [LabelWidth(WIDTH_LABEL)]
+        [ShowInInspector]
+        [PropertyOrder(-1)]
+        public string TypeFullName => GetType().FullName;
+
+        [TabGroup("Base/H1/V2", "Detail")]
+        [ListDrawerSettings(ShowFoldout = true, ShowItemCount = false, ShowPaging = false)]
+        [ShowInInspector]
+        [LabelText("继承关系")]
+        [LabelWidth(WIDTH_LABEL)]
+        [PropertyOrder(-1)]
+        public string[] InheritanceChain => GetType().GetInheritanceChain().Reverse().ToArray();
+#endif
+
+        public abstract float CalculateMagnitude(Entity specEntity, float magnitude);
+
+        public abstract void InitParameters(float[] floatParams,int[] intParams,string[] stringParams);
+    }
+}
