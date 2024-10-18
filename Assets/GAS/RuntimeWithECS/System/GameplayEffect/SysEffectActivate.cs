dca9f5896ed144780ef80a6bf65afe6132590db4
ex-hard <390178965@qq.com>
2024-10-18 18:13:45 +0800

    [feat] 完善系统

--- Assets/GAS/RuntimeWithECS/System/GameplayEffect/SysEffectActivate.cs
+++ Assets/GAS/RuntimeWithECS/System/GameplayEffect/SysEffectActivate.cs
@@ -1,4 +1,6 @@
-﻿using Unity.Burst;
+﻿using GAS.RuntimeWithECS.GameplayEffect.Component;
+using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 
 namespace GAS.RuntimeWithECS.System.GameplayEffect
@@ -10,13 +12,22 @@ namespace GAS.RuntimeWithECS.System.GameplayEffect
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
-            
+            state.RequireForUpdate<ComNeedActivate>();
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+            foreach (var (_,ge) in SystemAPI.Query<RefRO<ComNeedActivate>>().WithEntityAccess())
+            {
+                // TODO 激活GE的对应逻辑
+                // TODO 触发各种激活事件，回调
+                
+                // 完成任务后删除执行标签
+                ecb.RemoveComponent<ComNeedActivate>(ge);
+            }
+            ecb.Playback(state.EntityManager);
         }
 
         [BurstCompile]
--- Assets/GAS/RuntimeWithECS/System/GameplayEffect/SysEffectDeactivate.cs
+++ Assets/GAS/RuntimeWithECS/System/GameplayEffect/SysEffectDeactivate.cs
@@ -1,4 +1,6 @@
-﻿using Unity.Burst;
+﻿using GAS.RuntimeWithECS.GameplayEffect.Component;
+using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 
 namespace GAS.RuntimeWithECS.System.GameplayEffect
@@ -10,13 +12,22 @@ namespace GAS.RuntimeWithECS.System.GameplayEffect
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
-            
+            state.RequireForUpdate<ComNeedDeactivate>();
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+            foreach (var (_,ge) in SystemAPI.Query<RefRO<ComNeedDeactivate>>().WithEntityAccess())
+            {
+                // TODO 失活GE的对应逻辑
+                // TODO 触发各种失活事件，回调
+                
+                // 完成任务后删除执行标签
+                ecb.RemoveComponent<ComNeedDeactivate>(ge);
+            }
+            ecb.Playback(state.EntityManager);
         }
 
         [BurstCompile]
