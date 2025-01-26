02be44b8773b375f4428bec71468ec25ae4895b7
Ex-Hard <390178965@qq.com>
2025-01-27 00:08:59 +0800

    [feat] change

--- Assets/GAS/RuntimeWithECS/Ability/Component/Dynamic/CAbilityActive.cs
+++ Assets/GAS/RuntimeWithECS/Ability/Component/Dynamic/CAbilityActive.cs
@@ -2,6 +2,9 @@ using Unity.Entities;
 
 namespace GAS.RuntimeWithECS.Ability.Component.Dynamic
 {
+    /// <summary>
+    ///  能力激活标记【Tag Component】
+    /// </summary>
     public struct CAbilityActive : IComponentData
     {
         
--- Assets/GAS/RuntimeWithECS/Ability/GAUtil.cs
+++ Assets/GAS/RuntimeWithECS/Ability/GAUtil.cs
@@ -1,3 +1,7 @@
+using GAS.RuntimeWithECS.Ability.Component.Dynamic;
+using GAS.RuntimeWithECS.Core;
+using Unity.Entities;
+
 namespace GAS.RuntimeWithECS.Ability
 {
     public enum AbilityActivationResult
@@ -18,6 +22,62 @@ namespace GAS.RuntimeWithECS.Ability
     /// </summary>
     public static class GAUtil
     {
+        private static EntityManager _entityManager => GASManager.EntityManager;
         
+        /// <summary>
+        ///  检查能力是否可以激活
+        /// </summary>
+        /// <param name="ability"></param>
+        /// <returns>   </returns>
+        public static AbilityActivationResult CanActivateAbility(Entity ability)
+        {
+            if(_entityManager.HasComponent<CAbilityActive>(ability))
+                return AbilityActivationResult.FailHasActivated;
+            
+            if(!CheckGameplayTagsValidTpActivate(ability))
+                return AbilityActivationResult.FailTagRequirement;
+            
+            if (!CheckCost(ability)) 
+                return AbilityActivationResult.FailCost;
+            
+            if (!CheckCooldownReady(ability)) 
+                return AbilityActivationResult.FailCooldown;
+            
+            return AbilityActivationResult.Success;
+        }
+        
+        public static bool CheckGameplayTagsValidTpActivate(Entity ability)
+        {
+            // TODO
+            // var hasAllTags = Owner.HasAllTags(Ability.Tag.ActivationRequiredTags);
+            // var notHasAnyTags = !Owner.HasAnyTags(Ability.Tag.ActivationBlockedTags);
+            // var notBlockedByOtherAbility = true;
+            //
+            // foreach (var kv in Owner.AbilityContainer.AbilitySpecs())
+            // {
+            //     var abilitySpec = kv.Value;
+            //     if (abilitySpec.IsActive)
+            //         if (Ability.Tag.AssetTag.HasAnyTags(abilitySpec.Ability.Tag.BlockAbilitiesWithTags))
+            //         {
+            //             notBlockedByOtherAbility = false;
+            //             break;
+            //         }
+            // }
+            //
+            // return hasAllTags && notHasAnyTags && notBlockedByOtherAbility;
+            return true;
+        }
+        
+        public static bool CheckCost(Entity ability)
+        {
+            // TODO
+            return true;
+        }
+        
+        public static bool CheckCooldownReady(Entity ability)
+        {
+            // TODO
+            return true;
+        }
     }
 }
