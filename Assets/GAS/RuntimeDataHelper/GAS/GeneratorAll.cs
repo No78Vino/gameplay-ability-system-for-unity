aeb94149c98ac6aa3ab4ea78a0bc75e8fc835f8d
Ex-Hard <390178965@qq.com>
2025-04-06 23:09:59 +0800

    Update GeneratorAll.cs

--- Assets/GAS/RuntimeDataHelper/GAS/GeneratorAll.cs
+++ Assets/GAS/RuntimeDataHelper/GAS/GeneratorAll.cs
@@ -6,7 +6,7 @@ namespace GAS.Editor
 {
     public static class GeneratorAll
     {
-        [MenuItem("EX-GAS/CodeGenerate/Generate All")]
+        [MenuItem("EX-GAS/CodeGenerate/Generate All", priority = 0)]
         public static void Gen()
         {
             GeneratorAbilityCodeLib.Gen();
