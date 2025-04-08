207e46fc8489d5a35463e2e4705b7c3d1203d325
Ex-Hard <390178965@qq.com>
2025-04-08 23:41:11 +0800

    删除，移动脚本

--- Assets/GAS/ECS_TEST_RUNTIME_GEN_LIB/GameplayStackingCode.cs
+++
@@ -1,8 +0,0 @@
-namespace GAS.ECS_TEST_RUNTIME_GEN_LIB
-{
-    public static class GameplayStackingCode
-    {
-        public const int BurningState = 0;
-        public const int FrozenState = 1;
-    }
-}
