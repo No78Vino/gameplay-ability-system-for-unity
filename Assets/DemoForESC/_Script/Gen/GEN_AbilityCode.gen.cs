a2185b8c184820c29942f66af9d7026645da4a94
Ex-Hard <390178965@qq.com>
2025-04-13 21:37:17 +0800

    Update GEN_AbilityCode.gen.cs

--- Assets/DemoForESC/_Script/Gen/GEN_AbilityCode.gen.cs
+++ Assets/DemoForESC/_Script/Gen/GEN_AbilityCode.gen.cs
@@ -10,11 +10,14 @@ namespace GAS.Runtime
     public static class GEN_AbilityCode
     {
         public const int ABILITY_move = 1414245001;
+        public const int ABILITY_RunSpeedUp = -203323501;
 
         public static void LoadAbilityCode()
         {
             var ALMove = typeof(DemoForESC._Script.Gas.Ability.ALMove);
             GAS.RuntimeWithECS.Ability.AbilityHelper.RegisterAbilityLogic(ALMove.FullName, ALMove);
+            var ALApplyEffect = typeof(GAS.RuntimeWithECS.Ability.Component.CommonAbilityLogic.ALApplyEffect);
+            GAS.RuntimeWithECS.Ability.AbilityHelper.RegisterAbilityLogic(ALApplyEffect.FullName, ALApplyEffect);
             var ALDebugLog = typeof(GAS.RuntimeWithECS.Ability.Component.CommonAbilityLogic.ALDebugLog);
             GAS.RuntimeWithECS.Ability.AbilityHelper.RegisterAbilityLogic(ALDebugLog.FullName, ALDebugLog);
         }
