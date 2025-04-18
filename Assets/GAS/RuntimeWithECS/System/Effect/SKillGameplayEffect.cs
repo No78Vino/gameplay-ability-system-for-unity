f6609ba2a45e82e84e3b78b46f991e47dc4b6ef7
ex-hard <390178965@qq.com>
2025-04-18 10:35:13 +0800

    Update SKillGameplayEffect.cs

--- Assets/GAS/RuntimeWithECS/System/Effect/SKillGameplayEffect.cs
+++ Assets/GAS/RuntimeWithECS/System/Effect/SKillGameplayEffect.cs
@@ -1,5 +1,8 @@
 using GAS.Runtime;
+using GAS.RuntimeWithECS.Common.Component;
+using GAS.RuntimeWithECS.GameplayEffect.Component;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 
 namespace GAS.Runtime
@@ -11,13 +14,43 @@ namespace GAS.Runtime
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
-            
+            state.RequireForUpdate<CInUsage>();
+            state.RequireForUpdate<CEffectDestroy>();
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
 
+            foreach (var (_, ge) in SystemAPI.Query<RefRO<CEffectDestroy>>()
+                         .WithEntityAccess())
+            {
+                // 1.如果是持续型buff， 先尝试从asc的ge容器中移除
+                bool hasDuration = SystemAPI.HasComponent<CDuration>(ge);
+                if (hasDuration)
+                {
+                    bool hasInUsage = SystemAPI.HasComponent<CInUsage>(ge);
+                    if (hasInUsage)
+                    {
+                        var inUsage = SystemAPI.GetComponentRO<CInUsage>(ge);
+                        var targetAsc = inUsage.ValueRO.Target;
+                        var geContainer = SystemAPI.GetBuffer<BEGameplayEffect>(targetAsc);
+                        for (var i = 0; i < geContainer.Length; i++)
+                        {
+                            if (geContainer[i].GameplayEffect != ge) continue;
+                            geContainer.RemoveAt(i);
+                            break;
+                        }
+                    }
+                }
+
+                // 2.销毁ge
+                ecb.DestroyEntity(ge);
+            }
+
+            ecb.Playback(state.EntityManager);
+            ecb.Dispose();
         }
 
         [BurstCompile]
