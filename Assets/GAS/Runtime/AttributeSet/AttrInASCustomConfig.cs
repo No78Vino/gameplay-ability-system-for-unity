1a268d584f5ec8d5bf525865073a9d3e4c74a4f1
ex-hard <390178965@qq.com>
2025-04-28 16:19:21 +0800

    添加新版属性集属性参数类

---
+++ Assets/GAS/Runtime/AttributeSet/AttrInASCustomConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GAS.Runtime
+{
+    [Serializable]
+    public struct AttrInASCustomConfig
+    {
+        public int AttrCode;
+        public bool ClampMin;
+        public float ValueMin;
+        public bool ClampMax;
+        public float ValueMax;
+        public bool UseValueInit;
+        public float ValueDefaultInit;
+    }
+}
