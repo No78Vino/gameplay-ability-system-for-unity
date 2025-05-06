20e55fc6cf44b765c535378f50282a3b2498d8f3
ex-hard <390178965@qq.com>
2025-05-06 20:44:49 +0800

    Update ConfAssetCueOnAdd.cs

--- Assets/GAS/RuntimeDataHelper/GameplayEffect/EffectComponentConfigAsset/ConfAssetCueOnAdd.cs
+++ Assets/GAS/RuntimeDataHelper/GameplayEffect/EffectComponentConfigAsset/ConfAssetCueOnAdd.cs
@@ -1,12 +1,17 @@
 using System;
 using GAS.RuntimeWithECS.GameplayEffect;
 using GAS.RuntimeWithECS.GameplayEffect.Component;
+using Sirenix.OdinInspector;
 
 namespace GAS.RuntimeDataHelper.GameplayEffect.EffectComponentConfigAsset
 {
     [Serializable]
     public class ConfAssetCueOnAdd: BaseGameplayEffectComponentConfigAsset
     {
+        [TabGroup("Base","效果添加时Cue",SdfIconType.Clock)]
+        [LabelText("持续时间(-1=永久)")]
+        public float duration;
+        
         // TODO
         public override GameplayEffectComponentConfig GetConfig()
         {
