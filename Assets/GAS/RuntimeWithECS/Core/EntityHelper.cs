418a6e5c3b7f21dedd3c8c2b9e8411b8f1ebb472
ex-hard <390178965@qq.com>
2025-05-19 16:31:12 +0800

    Update EntityHelper.cs

--- Assets/GAS/RuntimeWithECS/Core/EntityHelper.cs
+++ Assets/GAS/RuntimeWithECS/Core/EntityHelper.cs
@@ -1,76 +1,163 @@
+using System.Collections.Generic;
 using Unity.Entities;
+using UnityEngine;
 
 namespace GAS.Runtime
 {
     public static class EntityHelper
     {
-        private static EntityManager _entityManager => GASManager.EntityManager;
         private static EntityCommandBuffer _ecb;
-        private static bool _usingEcb = false;
-        
+        private static bool _usingEcb;
+        private static EntityManager _entityManager => GASManager.EntityManager;
+
+        /// <summary>
+        ///     注册ECB
+        /// </summary>
+        /// <param name="ecb"></param>
         public static void RegisterEntityCommandBuffer(EntityCommandBuffer ecb)
         {
             _ecb = ecb;
             _usingEcb = true;
         }
-        
+
+        /// <summary>
+        ///     注销ECB
+        /// </summary>
         public static void UnregisterEntityCommandBuffer()
         {
             _ecb = default;
             _usingEcb = false;
         }
-        
-        public static void AddComponent<T>(Entity entity) where T : unmanaged,IComponentData
+
+        /// <summary>
+        ///     添加非托管组件
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void AddComponent<T>(Entity entity) where T : unmanaged, IComponentData
         {
-            if(_usingEcb)
+            if (_usingEcb)
                 _ecb.AddComponent<T>(entity);
             else
                 _entityManager.AddComponent<T>(entity);
         }
-        
-        public static void AddManagedComponent<T>(Entity entity) where T : class,IComponentData
+
+        /// <summary>
+        ///     添加托管组件
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void AddManagedComponent<T>(Entity entity) where T : class, IComponentData
         {
-            if(_usingEcb)
+            if (_usingEcb)
                 _ecb.AddComponent<T>(entity);
             else
                 _entityManager.AddComponent<T>(entity);
         }
-        
+
+        /// <summary>
+        ///     移除组件
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <typeparam name="T"></typeparam>
         public static void RemoveComponent<T>(Entity entity) where T : IComponentData
         {
-            if(_usingEcb)
+            if (_usingEcb)
                 _ecb.RemoveComponent<T>(entity);
             else
                 _entityManager.RemoveComponent<T>(entity);
         }
-        
-        public static void SetComponent<T>(Entity entity, T component) where T : unmanaged,IComponentData
+
+        /// <summary>
+        ///     设置非托管组件数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="component"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void SetComponent<T>(Entity entity, T component) where T : unmanaged, IComponentData
         {
-            if(_usingEcb)
-                _ecb.SetComponent(entity,component);
+            if (_usingEcb)
+                _ecb.SetComponent(entity, component);
             else
-                _entityManager.SetComponentData(entity,component);
+                _entityManager.SetComponentData(entity, component);
         }
-        
-        public static void SetManagedComponent<T>(Entity entity, T component) where T : class,IComponentData,new()
+
+        /// <summary>
+        ///     设置托管组件数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="component"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void SetManagedComponent<T>(Entity entity, T component) where T : class, IComponentData, new()
         {
-            if(_usingEcb)
-                _ecb.SetComponent(entity,component);
+            if (_usingEcb)
+                _ecb.SetComponent(entity, component);
             else
-                _entityManager.SetComponentData(entity,component);
+                _entityManager.SetComponentData(entity, component);
         }
-        
+
+        /// <summary>
+        ///     摧毁实例
+        /// </summary>
+        /// <param name="entity"></param>
         public static void DestroyEntity(Entity entity)
         {
-            if(_usingEcb)
+            if (_usingEcb)
                 _ecb.DestroyEntity(entity);
             else
                 _entityManager.DestroyEntity(entity);
         }
-        
+
+        /// <summary>
+        ///     设置能变组件状态
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="enable"></param>
+        /// <typeparam name="T"></typeparam>
         public static void SetComponentEnabled<T>(Entity entity, bool enable) where T : IEnableableComponent
         {
-            _entityManager.SetComponentEnabled<T>(entity,enable);
+            _entityManager.SetComponentEnabled<T>(entity, enable);
         }
+
+
+        #region GameObject绑定
+
+        private static readonly Dictionary<Entity, GameObject> _bindingGameObjects = new();
+
+        public static void ClearGameObjectBinding()
+        {
+            _bindingGameObjects.Clear();
+        }
+
+        /// <summary>
+        ///     绑定gameObject到entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="gameObject"></param>
+        public static void BindGameObjectToEntity(Entity entity, GameObject gameObject)
+        {
+            if (_entityManager.Exists(entity) && gameObject != null) _bindingGameObjects.Add(entity, gameObject);
+        }
+
+        /// <summary>
+        ///     解绑gameObject
+        /// </summary>
+        /// <param name="entity"></param>
+        public static void UnbindGameObjectToEntity(Entity entity)
+        {
+            if (_entityManager.Exists(entity)) _bindingGameObjects.Remove(entity);
+        }
+
+        /// <summary>
+        ///     获取entity绑定的gameObject
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public static GameObject GetGameObjectFromEntity(Entity entity)
+        {
+            return _bindingGameObjects.GetValueOrDefault(entity);
+        }
+
+        #endregion
     }
 }
