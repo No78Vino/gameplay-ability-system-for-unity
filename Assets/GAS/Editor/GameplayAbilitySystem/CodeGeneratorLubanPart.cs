79b623d2a95f4706db7c2f59e4524335e968dfc7
ex-hard <390178965@qq.com>
2025-10-09 18:48:39 +0800

    change

--- Assets/GAS/Editor/GameplayAbilitySystem/CodeGeneratorLubanPart.cs
+++ Assets/GAS/Editor/GameplayAbilitySystem/CodeGeneratorLubanPart.cs
@@ -14,6 +14,81 @@ namespace GAS.Editor
             writer.WriteLine("//// This is a generated file. ////");
             writer.WriteLine("////     Do not modify it.     ////");
             writer.WriteLine("///////////////////////////////////");
+
+            writer.WriteLine("");
+
+            writer.WriteLine("using System;");
+            writer.WriteLine("using System.Collections.Generic;");
+
+            writer.WriteLine("namespace GAS.Runtime");
+            writer.WriteLine("{");
+            writer.Indent++;
+            {
+                writer.WriteLine("public static class XLuban");
+                writer.WriteLine("{");
+                writer.Indent++;
+                {
+                    writer.WriteLine(
+                        $"public const string GAME_CONF_DIR = \"{GASSettingAsset.Instance.TableOutpuPath}\";");
+                    writer.WriteLine("private static Tables _tables;");
+                    writer.WriteLine("public static Tables Tables");
+                    writer.WriteLine("{");
+                    writer.Indent++;
+                    {
+                        writer.WriteLine("{");
+                        writer.WriteLine("get");
+                        writer.WriteLine("{");
+                        writer.WriteLine("if (_tables == null) LoadTables();");
+                        writer.WriteLine("return _tables;");
+                        writer.WriteLine("}");
+                        writer.WriteLine("}");
+                    }
+                    writer.Indent--;
+                    writer.WriteLine("}");
+
+                    // public static void LoadTables()
+                    // {
+                    //     if (_tables != null) return; // Already loaded
+                    //     _tables = new Tables(file => JSON.Parse(File.ReadAllText($"{GAME_CONF_DIR}/{file}.json")));
+                    // }
+
+
+
+
+                    // var allAbilityNames = GetAbilityNames();
+                    // foreach (var kv in allAbilityNames)
+                    // {
+                    //     var abilityName = kv.Value;
+                    //     var code = kv.Key;
+                    //     writer.WriteLine($"public const int ABILITY_{abilityName} = {code};");
+                    // }
+                    //
+                    // writer.WriteLine("");
+                    // writer.WriteLine("public static void LoadAbilityCode()");
+                    // writer.WriteLine("{");
+                    // writer.Indent++;
+                    // {
+                    //     var subTypes = EXEditorHelper.GetCachedAbilityLogicTypes();
+                    //     foreach (var subType in subTypes)
+                    //     {
+                    //         var typeFullName = subType.FullName;
+                    //         var shortTypeName = subType.Name;
+                    //         var abilityParamType =
+                    //             EXEditorHelper.GetCachedAbilityLogicToAbilityParamTypeMap()[typeFullName];
+                    //         var abilityParamTypeFullName = abilityParamType.FullName;
+                    //         writer.WriteLine($"var {shortTypeName} = typeof({typeFullName});");
+                    //         writer.WriteLine(
+                    //             $"GAS.RuntimeWithECS.AbilityHelper.RegisterAbilityLogic({shortTypeName}.FullName, {shortTypeName},typeof({abilityParamTypeFullName}));");
+                    //     }
+                    // }
+                    // writer.Indent--;
+                    // writer.WriteLine("}");
+                }
+                writer.Indent--;
+                writer.WriteLine("}");
+            }
+            writer.Indent--;
+            writer.WriteLine("}");
         }
     }
 }
