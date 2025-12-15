d5ce329bcc5aca5c1cf7daf71d21e311ce3f9154
ex-hard <390178965@qq.com>
2025-12-15 15:53:45 +0800

    change

---
+++ Assets/DemoForESC/_Script/Guide/GuideLearningKey.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoForESC._Script
+{
+    public enum GuideLearningKey
+    {
+        None,
+        Move,
+        Run,
+        Dodge,
+        SuperDash,
+        MeleeAttack,
+        SuperMeleeAttack,
+        GunAttack,
+        SuperGunAttack,
+    }
+}
