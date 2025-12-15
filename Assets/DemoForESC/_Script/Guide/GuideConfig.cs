55495e2e6799a543e62d92972e9d2a441a3f66ba
ex-hard <390178965@qq.com>
2025-12-15 15:30:41 +0800

    Update GuideConfig.cs

--- Assets/DemoForESC/_Script/Guide/GuideConfig.cs
+++ Assets/DemoForESC/_Script/Guide/GuideConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace DemoForESC._Script
 {
@@ -6,7 +7,24 @@ namespace DemoForESC._Script
     {
         public readonly static Dictionary<int, List<GuideInfo>> Data = new Dictionary<int, List<GuideInfo>>()
         {
-
+            [1] = new List<GuideInfo>()
+            {
+              new GuideInfo()
+              {
+                  title = "EX-HARD",
+                  content = "你好欢迎来到EX-GAS演示训练场。"
+              },
+              new GuideInfo()
+              {
+                  title = "EX-HARD",
+                  content = "接下来，我会为你进行简单的引导。"
+              },
+              new GuideInfo()
+              {
+                  title = "这是移动。",
+                  content = "按键：W/A/S/D"
+              },
+            },
         };
     }
 
@@ -14,5 +32,7 @@ namespace DemoForESC._Script
     {
         public string title;
         public string content;
+        public Action onBegin;
+        public Func<bool> checkFunction;
     }
 }
